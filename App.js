bdd3d20732c3affd4e9eb6aed18340cea4cc9c88
Anton Sipin <anton.sipin@gmail.com>
2026-08-14 11:34:06 +0300

    Update App.js

--- App.js
+++ App.js
@@ -2,11 +2,8 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
-
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-
 import MainStackNavigator from './navigation/StackNavigation';
 
 const Stack = createStackNavigator();
